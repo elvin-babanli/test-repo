c72849d60982b51c967967d12fa0326c06b5d62a
Elvin Babanli <elvinbabanli0@gmail.com>
2025-08-02 16:28:37 +0200

    New game first try

--- app.py
+++ app.py
@@ -1,3 +1,39 @@
-print("Hello GitHub")
-print("My name is Elvin!")
-print("I am a software engineer")
+import random
+
+while True:
+    game_choose = random.randint(1, 3)
+    option = int(input("Select 1.Rock, 2.Paper, 3.Scissors: "))
+
+    rock = 1
+    paper = 2
+    scissors = 3
+    if game_choose == rock and option == paper:
+        print("Computer choose: ",game_choose)
+        print("You win!")
+    elif game_choose == rock and option == scissors:
+        print("Computer choose: ",game_choose)
+        print("Computer wins!")
+    elif game_choose == rock and option == rock:
+        print("Computer choose: ",game_choose)
+        print("No win!")
+    elif game_choose == paper and option == rock:
+        print("Computer choose: ",game_choose)
+        print("Computer wins!")
+    elif game_choose == paper and option == paper:
+        print("Computer choose: ",game_choose)
+        print("No win!")
+    elif game_choose == paper and option == scissors:
+        print("Computer choose: ",game_choose)
+        print("You win!")
+    elif game_choose == scissors and option == rock:
+        print("Computer choose: ",game_choose)
+        print("You win!")
+    elif game_choose == scissors and option == paper:
+        print("Computer choose: ",game_choose)
+        print("Computer wins!")
+    elif game_choose == scissors and option == scissors:
+        print("Computer choose: ",game_choose)
+        print("No win!")
+
+
+
